8f29213be55af45f60329ae2b07498b78556ddc3
Kalliana <kalliana.glaeser@gmail.com>
2026-01-12 19:55:55 -0300

    continue for-while

---
+++ for-while/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main  {
+  void main (){
+    Scanner entrada = new Scanner(System.in);
+
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();;
+
+    while (x !=0) {
+      x = sc.nextInt();
+    }
+    sc.close();
+  }
+}
