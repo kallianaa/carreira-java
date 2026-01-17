5733f683d40c74d6aac6cf4769c3ac1947bf1d0b
Kalliana <kalliana.glaeser@gmail.com>
2026-01-17 20:24:00 -0300

    update main

--- for-while/src/Main.java
+++ for-while/src/Main.java
@@ -1,20 +1,12 @@
-import java.util.Scanner;
+
 
 public class Main  {
   void main (){
-    Scanner entrada = new Scanner(System.in);
-
-    Scanner sc = new Scanner(System.in);
-    int x = sc.nextInt();;
-
-    int soma = 0;
-    while (x !=0) {
-      soma = soma + x;
-      x = sc.nextInt();
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            System.out.printf("[%d %d]", i, j);
+        }
+        System.out.println();
     }
-
-    System.out.println((soma));
-
-    sc.close();
   }
 }
