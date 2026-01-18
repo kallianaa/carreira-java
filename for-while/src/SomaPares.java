0a7031e3ac3b7e486e40cb9c7f039d8fcac68cdf
Kalliana <kalliana.glaeser@gmail.com>
2026-01-18 16:43:39 -0300

    create soma pares

---
+++ for-while/src/SomaPares.java
@@ -0,0 +1,13 @@
+public class SomaPares {
+    void main (){
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if ( i % 2 == 0 ) {
+                soma = soma + i;
+
+            }
+        }
+        System.out.println("A soma dos números pares de 1 a 100 é: " + soma);
+    }
+}
