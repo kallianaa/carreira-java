d962feb6e543c608648ad98e2c6c586dcef348d6
Kalliana <kalliana.glaeser@gmail.com>
2026-01-20 21:25:13 -0300

    update main

--- for-while/src/Main.java
+++ for-while/src/Main.java
@@ -7,6 +7,7 @@ public class Main  {
             System.out.printf("[%d %d]", i, j);
         }
         System.out.println();
+        System.out.println("============================");
     }
   }
 }
